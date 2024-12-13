d1950207ebb9fe8243dac6bd54c9b11de9d50111
Thomas Wehr <derwehr@gmail.com>
2024-12-13 13:35:54 +0100

    add button to enlarge and shrink details container

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -12,6 +12,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Grid2 from '@mui/material/Grid2';
 import Energy from './Energy.tsx';
+import Button from '@mui/material/Button';
 
 // Define custom icons
 const defaultIcon = new L.Icon({
@@ -36,6 +37,7 @@ export default function Map() {
   const [buildings, setBuildings] = useState<BuildingType[]>([]);
   const [selectedBuilding, setSelectedBuilding] = useState<BuildingType | null>(null);
   const [selectedEnergy, setSelectedEnergy] = useState<EnergyType | null>(null);
+  const [isRightPaneLarge, setIsRightPaneLarge] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -59,6 +61,10 @@ export default function Map() {
     }
   }, [selectedBuilding]);
 
+  const togglePaneSize = () => {
+    setIsRightPaneLarge(!isRightPaneLarge);
+  };
+
   return (
     <Box>
       <Typography variant="h3" gutterBottom>
@@ -67,8 +73,13 @@ export default function Map() {
       <Typography variant="body1" paragraph>
         Created by the <a href="https://www.ti.rw.fau.de/">FAU Chair of Technical Information Systems</a> in cooperation with the <a href="https://www.scs.fraunhofer.de/">Fraunhofer Department for Risk and Location Analyses</a> for the research project <a href="#">Granergize</a>. Contact: <a href="mailto:thomas.wehr@fau.de">Thomas Wehr</a>
       </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', padding: 1 }}>
+        <Button variant="contained" onClick={togglePaneSize}>
+          {isRightPaneLarge ? 'Shrink Details' : 'Enlarge Details'}
+        </Button>
+      </Box>
       <Grid2 container spacing={2} sx={{ height: 'calc(100vh - 200px)' }}>
-        <Grid2 size={7} sx={{ height: '100%', overflow: 'auto' }}>
+        <Grid2 size={isRightPaneLarge ? 3 : 8} sx={{ height: '100%', overflow: 'auto' }}>
           <MapContainer
             className="map-container"
             center={[50.976558, 10.404674]}
@@ -98,7 +109,7 @@ export default function Map() {
             )}
           </MapContainer>
         </Grid2>
-        <Grid2 size={5} sx={{ height: '100%', overflow: 'auto' }}>
+        <Grid2 size={isRightPaneLarge ? 9 : 4} sx={{ height: '100%', overflow: 'auto' }}>
           {
             !selectedEnergy && 
             <Typography variant="h4">Select a marker to show details</Typography>
